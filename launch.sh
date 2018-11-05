b5160a1589d6fb04217e9c806b835641cae6fa8c
Vincent Lee <vincent.lee@outlook.com>
2018-11-04 22:15:19 -0600

    Clarify how to start the plugins

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-../panda/x86_64-softmmu/qemu-system-x86_64 -drive file=vm.img,format=qcow2 -m 3G -monitor stdio -net user,hostfwd=tcp::2222-:22 -net nic "$@" 
+../panda/x86_64-softmmu/qemu-system-x86_64 -drive file=vm.img,format=qcow2 -m 3G -monitor telnet::4444,server,nowait -net user,hostfwd=tcp::2222-:22 -net nic "$@" 
