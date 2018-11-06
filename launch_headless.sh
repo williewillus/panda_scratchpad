1d3f7fc94d4ff465471b52e419be15019701e442
jayashree <jaya@cs.utexas.edu>
2018-11-06 10:42:43 -0600

    change qemu launch script

--- launch_headless.sh
+++ launch_headless.sh
@@ -1,1 +1,11 @@
-./launch.sh -nographic "$@"
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage : ./launch_headless <path_to_img_file> <memory M|G>"
+	exit 1
+fi
+
+drive_path=$1
+memory=$2
+
+./launch.sh $drive_path $memory '-nographic'
